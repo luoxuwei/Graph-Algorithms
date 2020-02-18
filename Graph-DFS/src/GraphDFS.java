de9f03d7661e9c62ee20fb0224a5496b4b6c4393
luoxuwei <xwluo@foxmail.com>
2020-02-18 23:47:42 +0800

    修改初始遍历逻辑，处理图不是一个连通图有多个连通分量的情况

--- Graph-DFS/src/GraphDFS.java
+++ Graph-DFS/src/GraphDFS.java
@@ -9,7 +9,12 @@ public class GraphDFS {
         order = new LinkedList<>();
         validate = new boolean[g.V()];
         this.g =g;
-        dfs(0);
+//        dfs(0);
+        //当输入图不是一个连通图，有多个连通分量时，为保证每个顶点都能访问到，在最开始的时候就需要用一个循环遍历所有顶点
+        for (int v = 0; v < g.V(); v++) {
+            if (!validate[v])
+                dfs(v);
+        }
     }
 
     private void dfs(int v) {
