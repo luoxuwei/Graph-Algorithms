34fc35c594374a39cebc59ae194dc9f590e7dafc
luoxuwei <xwluo@foxmail.com>
2020-02-18 23:12:30 +0800

    创建图类，完成构造方法

---
+++ Graph-DFS/src/Graph.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Graph {
+
+    private int V;
+    private int E;
+    private TreeSet<Integer>[] adj;
+
+    public Graph(String path) {
+        File file = new File(path);
+
+        try (Scanner scanner = new Scanner(file)){
+            V = scanner.nextInt();
+            if (V < 0) throw new IllegalArgumentException("");
+            for (int i=0; i<V; i++)
+                adj[i] = new TreeSet<>();
+
+            E = scanner.nextInt();
+            for (int i=0; i<E; i++) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                //检测是否是平行边
+                if (!adj[a].contains(b))
+                    adj[a].add(b);
+
+                if (!adj[b].contains(a))
+                    adj[b].add(a);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
