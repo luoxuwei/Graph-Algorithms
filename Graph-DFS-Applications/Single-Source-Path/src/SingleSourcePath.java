481dbee2241a2a91df76d0aef36c421cec2131b1
luoxuwei <xwluo@foxmail.com>
2020-02-19 16:16:28 +0800

    完成单源路径遍历

--- Graph-DFS-Applications/Single-Source-Path/src/SingleSourcePath.java
+++ Graph-DFS-Applications/Single-Source-Path/src/SingleSourcePath.java
@@ -3,27 +3,30 @@ import java.util.List;
 import java.util.Stack;
 
 public class SingleSourcePath {
-    private List<Integer> order;
+    private int[] pre;
     private boolean[] validate;
     private Graph g;
-    public SingleSourcePath(Graph g) {
-        order = new LinkedList<>();
+
+    public SingleSourcePath(Graph g, int s) {
         validate = new boolean[g.V()];
         this.g =g;
+        pre = new int[g.V()];
+        for (int i = 0; i < g.V(); i++)
+            pre[i] = -1;
+        dfs(s, s);
     }
 
-    private void dfs(int v) {
-        order.add(v);
+    private void dfs(int v, int parent) {
         validate[v] = true;
+        pre[v] = parent;
         for (int w : g.adj(v)) {
             if (!validate[w])
-                dfs(w);
+                dfs(w, v);
         }
     }
 
     public static void main(String[] args) {
-        SingleSourcePath dfs = new SingleSourcePath(new Graph("g.txt"));
-        System.out.println(dfs.order);
+        SingleSourcePath dfs = new SingleSourcePath(new Graph("g.txt"), 0);
     }
 
 }
