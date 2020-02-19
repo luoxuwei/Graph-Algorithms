05770d59e48560b13f33f12887b5a8fbc1c70a9c
luoxuwei <xwluo@foxmail.com>
2020-02-19 18:54:26 +0800

    完成环检测算法

--- Graph-DFS-Applications/Cycle-Delection/src/CycleDetection.java
+++ Graph-DFS-Applications/Cycle-Delection/src/CycleDetection.java
@@ -1,33 +1,38 @@
 import java.util.*;
 
 public class CycleDetection {
-    private int[] pre;
     private boolean[] validate;
     private Graph g;
     private int s;
+    private boolean hasCycle;
 
-    public CycleDetection(Graph g, int s) {
+    public CycleDetection(Graph g) {
         validate = new boolean[g.V()];
         this.g =g;
         this.s = s;
-        pre = new int[g.V()];
-        for (int i = 0; i < g.V(); i++)
-            pre[i] = -1;
-        dfs(s, s);
+
+        for (int v = 0; v < g.V(); v++)
+            dfs(v, v);
     }
 
     private void dfs(int v, int parent) {
         validate[v] = true;
-        pre[v] = parent;
         for (int w : g.adj(v)) {
             if (!validate[w])
                 dfs(w, v);
+            else  if (w != v) {
+                hasCycle = true;
+            }
         }
     }
 
+    public boolean hasCycle() {
+        return hasCycle;
+    }
 
     public static void main(String[] args) {
-        CycleDetection dfs = new CycleDetection(new Graph("g.txt"), 0);
+        CycleDetection dfs = new CycleDetection(new Graph("g.txt"));
+        System.out.println(dfs.hasCycle());
     }
 
 }
