7590947f0f518c8a0853d63b18c8d6418b960a0e
luoxuwei <xwluo@foxmail.com>
2020-02-22 21:20:52 +0800

    leetcode 752 修改一些低级错误，很沮丧，为什么思维总是有错漏。提交还是失败，在处理一些极端case就好了，如["0000"], 8888

--- AI-Search-And-BFS/LeetCode-BFS/src/Solution752.java
+++ AI-Search-And-BFS/LeetCode-BFS/src/Solution752.java
@@ -8,15 +8,15 @@ public class Solution752 {
 
     public int openLock(String[] deadends, String target) {
         int targetInt = Integer.parseInt(target);
-        a = new int[]{0, -1, 1};
-        dirs = new int[4 * 3][4];
+        a = new int[]{-1, 1};
+        dirs = new int[4 * 2][4];
         LinkedList<Integer> queue = new LinkedList<>();
         int cur = 0;
         queue.addLast(0);
         TreeMap<Integer, Integer> validate = new TreeMap<>();
         int d = 0;
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < 2; j++) {
                 dirs[d][i] = a[j];
                 d++;
             }
@@ -29,39 +29,44 @@ public class Solution752 {
         int nextA = 0; int nextB = 0; int nextC = 0; int nextD = 0;
         int max = 9999 - deadends.length;
         int next;
+        int temp;
         validate.put(cur, 0);
         while (!queue.isEmpty()) {
             cur = queue.removeFirst();
             next = 0;
             for (int i = 0; i < dirs.length; i ++) {
-                 nextA = cur/1000 + dirs[i][0];
+                temp = cur;
+                 nextA = temp/1000 + dirs[i][0];
                  if (nextA < 0) {
                      nextA = 9;
                  } else if (nextA > 9) {
                      nextA = 0;
                  }
                  next = nextA *1000;
-                 nextB = cur/100 + dirs[i][1];
+                temp = temp - (temp/1000) * 1000;
+                 nextB = temp/100 + dirs[i][1];
                 if (nextB < 0) {
                     nextB = 9;
                 } else if (nextB > 9) {
                     nextB = 0;
                 }
-                next += nextB * 100;
-                 nextC = cur/10 + dirs[i][2];
+                next = next + nextB * 100;
+                temp = temp - (temp/100)*100;
+                 nextC = temp/10 + dirs[i][2];
                 if (nextC < 0) {
                     nextC = 9;
                 } else if (nextC > 9) {
                     nextC = 0;
                 }
-                next += nextC * 10;
-                 nextD = cur%1000 + dirs[i][3];
+                next = next + nextC * 10;
+                temp = temp - (temp/10)*10;
+                 nextD = temp + dirs[i][3];
                 if (nextD < 0) {
                     nextD = 9;
                 } else if (nextD > 9) {
                     nextD = 0;
                 }
-                next += nextD;
+                next = next + nextD;
                 if (!deadendsInt.contains(next) && !validate.containsKey(next)) {
                     queue.addLast(next);
                     validate.put(next, validate.get(cur) + 1);
