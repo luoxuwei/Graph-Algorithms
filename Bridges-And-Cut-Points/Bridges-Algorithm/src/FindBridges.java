411bb17533ab11003567166cf8016c4905b35a5c
luoxuwei <xwluo@foxmail.com>
2020-02-23 19:46:58 +0800

    寻找桥的非递归实现还有bug，先放一边。

--- Bridges-And-Cut-Points/Bridges-Algorithm/src/FindBridges.java
+++ Bridges-And-Cut-Points/Bridges-Algorithm/src/FindBridges.java
@@ -29,18 +29,19 @@ public class FindBridges {
     private void dfs(int v) {
         Stack<Integer> vstack = new Stack<>();
         Stack<LinkedList<Integer>> wstack = new Stack<>();
-        vstack.push(v);
-        validate[v] = true;
         int cur = v;
         int parent = v;
         int chil = v ;
         LinkedList<Integer> vlist;
-        while (!vstack.empty()) {
+        while (cur >= 0) {
             if (!validate[cur]) {
+                if (!vstack.isEmpty()) {
+                    parent = vstack.peek();
+                }
                 vstack.push(cur);
-                cnt++;
                 order[cur] = cnt;
                 low[cur] = cnt;
+                cnt++;
                 validate[cur] = true;
                 vlist = new LinkedList<>();
                 for (int w : G.adj(v)) {
@@ -52,18 +53,46 @@ public class FindBridges {
                 }
                 wstack.push(vlist);
             }
-            vlist = wstack.peek();
-            if (vlist.isEmpty()) {
-                wstack.pop();
-                chil = vstack.pop();
-                parent = vstack.peek();
-                low[parent] = Math.min(low[parent], low[chil]);
-                if (low[chil] > low[parent]) {
-                    res.add(new Pair<>(parent, chil));
-                }
+            cur = -1;
+            while (!wstack.isEmpty()) {
                 vlist = wstack.peek();
+                if (vlist.isEmpty()) {
+                    wstack.pop();
+                    chil = vstack.pop();
+                    if (!vstack.isEmpty()) {
+                        parent = vstack.peek();
+                        low[parent] = Math.min(low[parent], low[chil]);
+                        if (low[chil] > order[parent]) {
+                            res.add(new Pair<>(parent, chil));
+                        }
+                    }
+                } else {
+                    cur = vlist.removeFirst();
+                    break;
+                }
+
             }
-            cur = vlist.removeFirst();
+
         }
+    }
+
+    public List<Pair<Integer, Integer>> result() {
+        return res;
+    }
+
+    public static void main(String[] args){
+
+        Graph g = new Graph("g.txt");
+        FindBridges fb = new FindBridges(g);
+        System.out.println("Bridges in g : " + fb.result());
+
+        Graph g2 = new Graph("g2.txt");
+        FindBridges fb2 = new FindBridges(g2);
+        System.out.println("Bridges in g2 : " + fb2.result());
+
+        Graph g3 = new Graph("g3.txt");
+        FindBridges fb3 = new FindBridges(g3);
+        System.out.println("Bridges in g3 : " + fb3.result());
+
     }
 }
