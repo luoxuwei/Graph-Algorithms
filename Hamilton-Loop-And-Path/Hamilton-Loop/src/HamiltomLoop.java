e9908afabca89a9065e541ca686ed1c16f156545
luoxuwei <xwluo@foxmail.com>
2020-02-24 22:09:22 +0800

    完成哈密尔顿回路验证算法

--- Hamilton-Loop-And-Path/Hamilton-Loop/src/HamiltomLoop.java
+++ Hamilton-Loop-And-Path/Hamilton-Loop/src/HamiltomLoop.java
@@ -5,17 +5,29 @@ public class HamiltomLoop {
     public HamiltomLoop(Graph g) {
         G = g;
         validate = new boolean[g.V()];
-
-
+        dfs(0);
     }
 
-    private void dfs(int v) {
+    private boolean dfs(int v) {
         validate[v] = true;
 
         for (int w : G.adj(v)) {
             if (!validate[w]) {
-                dfs(w);
+                if (dfs(w)) return true;
+            } else if (w == 0 && allVisited()) {
+                return true;
             }
         }
+        //遍历了所有连接的顶点没有找到哈密尔顿回路，回退
+        validate[v] = false;
+        return false;
+    }
+
+    private boolean allVisited() {
+        for (boolean visited :validate) {
+            if (!visited) return false;
+        }
+
+        return true;
     }
 }
