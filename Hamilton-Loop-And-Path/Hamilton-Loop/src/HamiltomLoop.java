c30ec86c700142bc5aac8a9f1196709ec5618ad1
luoxuwei <xwluo@foxmail.com>
2020-02-24 22:25:38 +0800

    记录哈密尔顿回路路径并添加测试用例

--- Hamilton-Loop-And-Path/Hamilton-Loop/src/HamiltomLoop.java
+++ Hamilton-Loop-And-Path/Hamilton-Loop/src/HamiltomLoop.java
@@ -1,20 +1,29 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class HamiltomLoop {
     private Graph G;
     private boolean[] validate;
+    private int end = -1;
+    private int[] pre;
 
     public HamiltomLoop(Graph g) {
         G = g;
         validate = new boolean[g.V()];
-        dfs(0);
+        pre = new int[g.V()];
+        dfs(0, 0);
     }
 
-    private boolean dfs(int v) {
+    private boolean dfs(int v, int p) {
         validate[v] = true;
-
+        pre[v] = p;
         for (int w : G.adj(v)) {
             if (!validate[w]) {
-                if (dfs(w)) return true;
+                if (dfs(w, v)) return true;
             } else if (w == 0 && allVisited()) {
+                //一开始写成了 end = w，切结粗心大意
+                end = v;
                 return true;
             }
         }
@@ -29,5 +38,29 @@ public class HamiltomLoop {
         }
 
         return true;
+    }
+
+    public List<Integer> result() {
+        List<Integer> res = new LinkedList<>();
+        if (end == -1) {
+            return res;
+        }
+
+        res.add(end);
+        int cur = end;
+        while ((cur = pre[cur]) != 0) {
+            res.add(cur);
+        }
+        res.add(0);
+        Collections.reverse(res);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        HamiltomLoop hamiltomLoop = new HamiltomLoop(new Graph("g.txt"));
+        System.out.println(hamiltomLoop.result());
+
+        HamiltomLoop hamiltomLoop1 = new HamiltomLoop(new Graph("g2.txt"));
+        System.out.println(hamiltomLoop1.result());
     }
 }
