bd444cec89a8beadbb4a620f80945a852cd6e1b4
luoxuwei <xuwei.luo@nio.com>
2020-02-27 11:41:51 +0800

    改造成带权图

--- Minimum-Tree-Spanning/Weighted-Graph/src/Graph.java
+++ Minimum-Tree-Spanning/Weighted-Graph/src/Graph.java
@@ -1,13 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.List;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Graph {
     int E;
     int V;
-    TreeSet<Integer>[] adj;
+    TreeMap<Integer, Integer>[] adj;
 
     public Graph(String path) {
         File file = new File(path);
@@ -16,24 +14,25 @@ public class Graph {
 
             V = scanner.nextInt();
             E = scanner.nextInt();
-            adj = new TreeSet[V];
+            adj = new TreeMap[V];
 
             for (int i = 0; i < V; i++) {
-                adj[i] = new TreeSet();
+                adj[i] = new TreeMap();
             }
-            int a = 0; int b = 0;
+            int a = 0; int b = 0; int weight = 0;
             for (int i = 0; i < E; i++) {
                 a = scanner.nextInt();
                 b = scanner.nextInt();
+                weight = scanner.nextInt();
                 validateVertex(a);
                 validateVertex(b);
                 if (a == b)
                     throw new IllegalArgumentException("");//自环边
-                if (adj[a].contains(b) || adj[b].contains(a))
+                if (adj[a].containsKey(b) || adj[b].containsKey(a))
                     throw new IllegalArgumentException("");//平行边
 
-                adj[a].add(b);
-                adj[b].add(a);
+                adj[a].put(b, weight);
+                adj[b].put(a, weight);
             }
 
         } catch (FileNotFoundException e) {
@@ -57,7 +56,7 @@ public class Graph {
     }
 
     public Iterable<Integer> adj(int v) {
-        return adj[v];
+        return adj[v].keySet();
     }
 
     public int degree(int v) {
@@ -67,7 +66,7 @@ public class Graph {
     public void removeEdge(int v, int w) {
         validateVertex(v);
         validateVertex(w);
-        if (adj[v].contains(w)) E--;
+        if (adj[v].containsKey(w)) E--;
         adj[v].remove(w);
         adj[w].remove(v);
     }
@@ -76,11 +75,11 @@ public class Graph {
     public Graph clone() {
         try {
             Graph clone = (Graph) super.clone();
-            clone.adj = new TreeSet[clone.V];
+            clone.adj = new TreeMap[clone.V];
             for (int i = 0; i < clone.V; i++) {
-                clone.adj[i] = new TreeSet<Integer>();
-                for (int w : adj[i]) {
-                    clone.adj[i].add(w);
+                clone.adj[i] = new TreeMap<Integer, Integer>();
+                for (Map.Entry<Integer, Integer> entry: adj[i].entrySet()) {
+                    clone.adj[i].put(entry.getKey(), entry.getValue());
                 }
             }
 
