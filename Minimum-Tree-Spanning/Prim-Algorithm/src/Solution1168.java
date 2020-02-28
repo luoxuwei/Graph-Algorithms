f69c34e363b5a6d5db994c448cb3cc6fc0d13058
luoxuwei <xuwei.luo@nio.com>
2020-02-28 15:34:24 +0800

    开始解决 leetcode 1168 沿用部分辅助类的代码。之后还需要改造下。

---
+++ Minimum-Tree-Spanning/Prim-Algorithm/src/Solution1168.java
@@ -0,0 +1,164 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Solution1168 {
+    class WeightedEdge {
+        int v;
+        int w;
+        int weight;
+        WeightedEdge(int v, int w, int weight) {
+            this.v = v;
+            this.w = w;
+            this.weight = weight;
+        }
+    }
+
+    public class Graph {
+        int E;
+        int V;
+        TreeMap<Integer, Integer>[] adj;
+
+        public Graph(String path) {
+            File file = new File(path);
+
+            try (Scanner scanner = new Scanner(file)) {
+
+                V = scanner.nextInt();
+                E = scanner.nextInt();
+                adj = new TreeMap[V];
+
+                for (int i = 0; i < V; i++) {
+                    adj[i] = new TreeMap();
+                }
+                int a = 0; int b = 0; int weight = 0;
+                for (int i = 0; i < E; i++) {
+                    a = scanner.nextInt();
+                    b = scanner.nextInt();
+                    weight = scanner.nextInt();
+                    validateVertex(a);
+                    validateVertex(b);
+                    if (a == b)
+                        throw new IllegalArgumentException("");//自环边
+                    if (adj[a].containsKey(b) || adj[b].containsKey(a))
+                        throw new IllegalArgumentException("");//平行边
+
+                    adj[a].put(b, weight);
+                    adj[b].put(a, weight);
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+        public void validateVertex(int v) {
+            if (v < 0 || v >= V)
+                throw new IllegalArgumentException("");
+        }
+
+        public int V() {
+            return V;
+        }
+
+        public int E() {
+            return E;
+        }
+
+        public Iterable<Integer> adj(int v) {
+            return adj[v].keySet();
+        }
+
+        public int degree(int v) {
+            return adj[v].size();
+        }
+
+        public int getWeight(int v, int w) {
+            if (hasEdge(v, w)) return adj[v].get(w);
+            throw new IllegalArgumentException("");
+        }
+
+        public boolean hasEdge(int v, int w) {
+            validateVertex(v);
+            validateVertex(w);
+            return adj[v].containsKey(w);
+        }
+
+        public void removeEdge(int v, int w) {
+            validateVertex(v);
+            validateVertex(w);
+            if (adj[v].containsKey(w)) E--;
+            adj[v].remove(w);
+            adj[w].remove(v);
+        }
+
+        @Override
+        public Graph clone() {
+            try {
+                Graph clone = (Graph) super.clone();
+                clone.adj = new TreeMap[clone.V];
+                for (int i = 0; i < clone.V; i++) {
+                    clone.adj[i] = new TreeMap<Integer, Integer>();
+                    for (Map.Entry<Integer, Integer> entry: adj[i].entrySet()) {
+                        clone.adj[i].put(entry.getKey(), entry.getValue());
+                    }
+                }
+
+                return clone;
+
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+    }
+
+    public class CC {
+        Graph G;
+        int cccount;
+        int[] ccids;
+
+        public CC(Graph g) {
+            G = g;
+            cccount = 0;
+            ccids = new int[g.V()];
+            for (int i = 0; i < g.V(); i++)
+                ccids[i] = -1;
+
+            for (int v = 0; v < G.V(); v++) {
+                if (ccids[v] == -1) {
+                    dfs(v, cccount);
+                    cccount++;
+                }
+            }
+        }
+
+        private void dfs(int v, int ccid) {
+            ccids[v] = ccid;
+
+            for (int w: G.adj(v)) {
+                if (ccids[w] == -1) {
+                    dfs(w, ccid);
+                }
+            }
+        }
+
+        public int cccount() {
+            return cccount;
+        }
+
+
+    }
+
+    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+
+    }
+
+
+}
