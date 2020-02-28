d9ed8ee65f0a7ea0e9ae72c7fc002a2f1b176274
luoxuwei <xwluo@foxmail.com>
2020-02-28 21:59:42 +0800

    实现dijkstra算法，找到当然没被遍历的顶点中路径最短的顶点

--- Shortest-Path/Dijkstra-Algorithm/src/Dijkstra.java
+++ Shortest-Path/Dijkstra-Algorithm/src/Dijkstra.java
@@ -12,5 +12,19 @@ public class Dijkstra {
         dis = new int[g.V()];
         Arrays.fill(dis, Integer.MAX_VALUE);
         visited = new boolean[g.V()];
+        dis[s] = 0;
+        while (true) {
+            int cur = -1;
+            int minDis = Integer.MIN_VALUE;
+            for (int v = 0; v < g.V(); v++) {
+                if (!visited[v] && dis[v] < minDis) {
+                    minDis = dis[v];
+                    cur = v;
+                }
+            }
+
+            if (cur == -1) break;
+            
+        }
     }
 }
