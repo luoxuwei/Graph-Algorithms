99e2cdbc8144d5a1848fb112e309fe16802bde47
luoxuwei <xwluo@foxmail.com>
2020-02-29 23:25:18 +0800

    完成FLoyed算法的遍历过程

--- Shortest-Path/Floyed-Algorithm/src/FloyedAlgorithm.java
+++ Shortest-Path/Floyed-Algorithm/src/FloyedAlgorithm.java
@@ -9,6 +9,16 @@ public class FloyedAlgorithm {
         dis = new int[G.V()][G.V()];
         for (int i = 0; i < dis.length; i++)
             Arrays.fill(dis[i], Integer.MAX_VALUE);
-        
+
+        for(int t = 0; t < G.V(); t++) {
+            for (int v = 0; v < G.V(); v++) {
+                for (int w = 0; w < G.V(); w++) {
+                    if (dis[v][t] != Integer.MAX_VALUE && dis[t][w] != Integer.MAX_VALUE
+                            && dis[v][t] + dis[t][w] < dis[v][w]) {
+                        dis[v][w] = dis[v][t] + dis[t][w];
+                    }
+                }
+            }
+        }
     }
 }
