a7fa6fd7a00db579f652b2684392d19c55690262
luoxuwei <xwluo@foxmail.com>
2020-02-29 17:14:24 +0800

    开始实现Bellmen-Ford算法，完成最短路径计算

--- Shortest-Path/Bellmen-Ford-Algorithm/src/BellmenFordAlgorithm.java
+++ Shortest-Path/Bellmen-Ford-Algorithm/src/BellmenFordAlgorithm.java
@@ -5,11 +5,26 @@ public class BellmenFordAlgorithm {
     int[] dis;
     int[] pre;
     boolean hasNegCycle;
-    public BellmenFordAlgorithm(Graph g) {
+    public BellmenFordAlgorithm(Graph g, int s) {
         G = g;
         dis = new int[g.V()];
         pre = new int[g.V()];
         Arrays.fill(pre, -1);
         Arrays.fill(dis, Integer.MAX_VALUE);
+
+        dis[s] = 0;
+        //对每个顶点进行v-1轮松弛操作
+        for(int pass = 0; pass < G.V() - 1; pass++) {
+
+            //对每个顶点进行一轮松弛操作
+            for (int v = 0; v < G.V(); v++) {
+                for (int w : G.adj(v)) {
+                    if (dis[v] != Integer.MAX_VALUE && dis[v] + G.getWeight(v, w) < dis[w]) {
+                        dis[w] = dis[v] + G.getWeight(v, w);
+                    }
+
+                }
+            }
+        }
     }
 }
