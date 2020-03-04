8c5d0df31f8e993a6f611c96ba43529dcba9343b
luoxuwei <xuwei.luo@nio.com>
2020-03-04 15:59:19 +0800

    leetcode 1168 完成kruskal版本的解

--- Minimum-Tree-Spanning/Prim-Algorithm/src/Solution1168.java
+++ Minimum-Tree-Spanning/Prim-Algorithm/src/Solution1168.java
@@ -1,12 +1,105 @@
+import javafx.util.Pair;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Solution1168 {
+
+    class UF {
+        Pair<Integer, Integer>[] nodes;
+
+        public UF(int[] wells) {
+            nodes = new Pair[wells.length];
+            for (int i = 0; i<wells.length; i++) {
+                nodes[i] = new Pair<>(i, wells[i]);
+            }
+        }
+
+        public Pair<Integer, Integer> find(int v) {
+            if (nodes[v].getKey() != v)
+                return find(nodes[v].getKey());
+
+            return nodes[v];
+        }
+
+        public void unionElement(int a, int b) {
+            Pair<Integer, Integer> rootA = find(a);
+            Pair<Integer, Integer> rootB = find(b);
+            nodes[rootB.getKey()] = rootA;
+        }
+    }
+
+    public class WeightedEdge implements Comparable<WeightedEdge>{
+        private int V;
+        private int W;
+        private int Weight;
+
+        public WeightedEdge(int v, int w, int weight) {
+            V = v;
+            W = w;
+            Weight = weight;
+        }
+
+        public int getV() {
+            return V;
+        }
+
+        public int getW() {
+            return W;
+        }
+
+        public int getWeight() {
+            return Weight;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%d-%d:%d", V, W, Weight);
+        }
+
+        @Override
+        public int compareTo(WeightedEdge o) {
+            return Weight - o.Weight;
+        }
+    }
 
 
 
     public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+        return kruskal(n, wells, pipes);
+    }
 
+    private int kruskal(int n, int[] wells, int[][] pipies) {
 
+        int cost = 0;
+        for (int c:wells) {
+            cost = cost + c;
+        }
+        UF uf = new UF(wells);
+        List<WeightedEdge> weightedEdgeList = new ArrayList<>();
+        for (int i = 0; i < pipies.length; i++) {
+            weightedEdgeList.add(new WeightedEdge(pipies[i][0], pipies[i][1], pipies[i][2]));
+        }
+
+        Collections.sort(weightedEdgeList);
+        for (WeightedEdge weightedEdge:weightedEdgeList) {
+
+            Pair<Integer, Integer> a = uf.find(weightedEdge.V-1);
+            Pair<Integer, Integer> b = uf.find(weightedEdge.W-1);
+            if (a.getKey() != b.getKey()) {
+                if (a.getValue() >= b.getValue() && weightedEdge.getWeight() < a.getValue()) {
+                    cost = cost - a.getValue() + weightedEdge.getWeight();
+                    uf.unionElement(b.getKey(), a.getKey());
+                } else if (b.getValue() > weightedEdge.getWeight()) {
+                    cost = cost - b.getValue() + weightedEdge.getWeight();
+                    uf.unionElement(a.getKey(), b.getKey());
+                }
+            }
+
+        }
+
+        return cost;
 
     }
 
