696fd65afe674170a81be3b0ceb9097a612e25b7
luoxuwei <xwluo@foxmail.com>
2020-03-07 16:56:09 +0800

    开始实现Edmonds-Krap算法，改造图类

--- Network-Flow/Edmonds-Karp-Algorithm/src/Graph.java
+++ Network-Flow/Edmonds-Karp-Algorithm/src/Graph.java
@@ -6,14 +6,16 @@ public class Graph {
     int E;
     int V;
     TreeMap<Integer, Integer>[] adj;
+    boolean directed;
 
-    public Graph(String path) {
+    public Graph(String path, boolean directed) {
         File file = new File(path);
-
+        this.directed = directed;
         try (Scanner scanner = new Scanner(file)) {
 
             V = scanner.nextInt();
-            E = scanner.nextInt();
+            E = 0;
+            scanner.nextInt();
             adj = new TreeMap[V];
 
             for (int i = 0; i < V; i++) {
@@ -24,15 +26,7 @@ public class Graph {
                 a = scanner.nextInt();
                 b = scanner.nextInt();
                 weight = scanner.nextInt();
-                validateVertex(a);
-                validateVertex(b);
-                if (a == b)
-                    throw new IllegalArgumentException("");//自环边
-                if (adj[a].containsKey(b) || adj[b].containsKey(a))
-                    throw new IllegalArgumentException("");//平行边
-
-                adj[a].put(b, weight);
-                adj[b].put(a, weight);
+                addEdge(a, b, weight);
             }
 
         } catch (FileNotFoundException e) {
@@ -40,6 +34,34 @@ public class Graph {
         }
 
 
+    }
+
+    public Graph(String path) {
+        this(path, false);
+    }
+
+    public Graph(int v, boolean directed) {
+        V = v;
+        this.directed = directed;
+        E = 0;
+        adj = new TreeMap[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new TreeMap();
+        }
+    }
+
+    public void addEdge(int a, int b, int weight) {
+        validateVertex(a);
+        validateVertex(b);
+        if (a == b)
+            throw new IllegalArgumentException("");//自环边
+        if (adj[a].containsKey(b) || adj[b].containsKey(a))
+            throw new IllegalArgumentException("");//平行边
+
+        adj[a].put(b, weight);
+        if (directed)
+            adj[b].put(a, weight);
+        E++;
     }
 
     public void validateVertex(int v) {
@@ -59,8 +81,15 @@ public class Graph {
         return adj[v].keySet();
     }
 
-    public int degree(int v) {
-        return adj[v].size();
+
+    public void setWeight(int a, int b, int w) {
+        if (!hasEdge(a, b))
+            throw new IllegalArgumentException("");
+
+        adj[a].put(b, w);
+        if (!directed)
+            adj[b].put(a, w);
+
     }
 
     public int getWeight(int v, int w) {
@@ -79,7 +108,8 @@ public class Graph {
         validateVertex(w);
         if (adj[v].containsKey(w)) E--;
         adj[v].remove(w);
-        adj[w].remove(v);
+        if (!directed)
+            adj[w].remove(v);
     }
 
     @Override
