ffc18fc08cb8536cbe9645ed2d843b80e8e63145
luoxuwei <xuwei.luo@nio.com>
2020-03-11 11:19:02 +0800

    实现有向图的拓扑排序，第一轮遍历找到入度为0的顶点

--- Directed-Graph/Topological-Sort/src/TopoSort.java
+++ Directed-Graph/Topological-Sort/src/TopoSort.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class TopoSort {
     Graph G;
-    
+    int[] outdegrees;
+    ArrayList<Integer> res;
     public TopoSort(Graph g) {
         G = g;
+        outdegrees = new int[G.V()];
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int v = 0; v < G.V(); v++) {
+            outdegrees[v] = G.outdegree(v);
+            if (outdegrees[v] == 0) {
+                queue.addLast(v);
+            }
+        }
 
     }
 }
