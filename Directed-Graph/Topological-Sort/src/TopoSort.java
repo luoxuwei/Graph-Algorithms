73970d5893a8dc6ddcae1cb88c1c76d79db31c69
luoxuwei <xuwei.luo@nio.com>
2020-03-11 11:45:01 +0800

    完成有向图拓扑排序算法

--- Directed-Graph/Topological-Sort/src/TopoSort.java
+++ Directed-Graph/Topological-Sort/src/TopoSort.java
@@ -3,18 +3,36 @@ import java.util.LinkedList;
 
 public class TopoSort {
     Graph G;
-    int[] outdegrees;
+    int[] indegrees;
     ArrayList<Integer> res;
     public TopoSort(Graph g) {
         G = g;
-        outdegrees = new int[G.V()];
+        res = new ArrayList<>();
+        indegrees = new int[G.V()];
         LinkedList<Integer> queue = new LinkedList<>();
         for (int v = 0; v < G.V(); v++) {
-            outdegrees[v] = G.outdegree(v);
-            if (outdegrees[v] == 0) {
+            indegrees[v] = G.indegree(v);
+            if (indegrees[v] == 0) {
                 queue.addLast(v);
             }
         }
+        int cur = 0;
+        while (!queue.isEmpty()) {
+            cur = queue.removeFirst();
+            res.add(cur);
+            for (int w : G.adj(cur)) {
+                indegrees[w]--;
+                if (indegrees[w] == 0) {
+                    queue.addLast(w);
+                }
+            }
+        }
 
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph("ug.txt", true);
+        TopoSort sort = new TopoSort(g);
+        System.out.println(sort.res);
     }
 }
